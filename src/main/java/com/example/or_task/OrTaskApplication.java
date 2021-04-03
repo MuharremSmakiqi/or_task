ca68ecec43f9db9b1eff3b78b4ee15bbdb300613
Muharrem <muharrem.w2@gmail.com>
2021-04-03 13:04:49 +0200

    setting up docker and run the application

--- src/main/java/com/example/or_task/OrTaskApplication.java
+++ src/main/java/com/example/or_task/OrTaskApplication.java
@@ -2,10 +2,17 @@ package com.example.or_task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OrTaskApplication {
 
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World -ps";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(OrTaskApplication.class, args);
 	}
